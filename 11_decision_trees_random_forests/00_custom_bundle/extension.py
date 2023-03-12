bc1b5aa03452aab5f2f7e42bd92f24d02f9cefbd
jzkoh2010 <junzhi.koh@gmail.com>
2023-03-12 23:01:23 +0800

    Update extension.py
    for NYSE

--- 11_decision_trees_random_forests/00_custom_bundle/extension.py
+++ 11_decision_trees_random_forests/00_custom_bundle/extension.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 sys.path.append(Path('~', '.zipline').expanduser().as_posix())
 from zipline.data.bundles import register
-from stooq_jp_stocks import stooq_jp_to_bundle
+from stooq_us_stocks import stooq_jp_to_bundle
 from datetime import time
 from pytz import timezone
 
 
 register('stooq',
          stooq_jp_to_bundle(),
-         calendar_name='XTKS',
+         calendar_name='XNYS',
          )
 
 
